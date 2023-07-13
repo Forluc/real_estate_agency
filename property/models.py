d3a7caa20efd2891d7dcb8e5f66cea16f9525e0e
Dmitrii Masliaev <m6ytcbi@gmail.com>
2023-07-13 11:55:31 +0300

    fill with data field Flat.construction_year

--- property/models.py
+++ property/models.py
@@ -46,7 +46,9 @@ class Flat(models.Model):
         null=True,
         blank=True,
         db_index=True)
-    new_building = models.BooleanField(choices=[(True, 'Новостройка'), (False, 'Старое здание')], blank=True, null=True)
+
+    new_building = models.BooleanField(
+        choices=[(True, 'Новостройка'), (False, 'Старое здание')], blank=True, null=True)
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
