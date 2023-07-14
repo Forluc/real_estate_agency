bc5a28989b5f711003b8c794bd64f604a561724e
Dmitrii Masliaev <m6ytcbi@gmail.com>
2023-07-14 11:43:10 +0300

    add valid phonenumbers to owner_pure_phone

--- property/models.py
+++ property/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.utils import timezone
 from phonenumber_field.modelfields import PhoneNumberField
 
+
 class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
     owner_pure_phone = PhoneNumberField('Нормализованный номер владельца', blank=True, db_index=True)
