80e79260efa9fab303b9608edbbcc308b6d648b0
Dmitrii Masliaev <m6ytcbi@gmail.com>
2023-07-20 13:58:13 +0300

    rename related_name for Owner.flats

--- property/models.py
+++ property/models.py
@@ -71,7 +71,7 @@ class Owner(models.Model):
     name = models.CharField('ФИО владельца', max_length=200, db_index=True)
     phone = models.CharField('Номер владельца', max_length=20, db_index=True)
     pure_phone = PhoneNumberField('Нормализованный номер владельца', blank=True, db_index=True)
-    flats = models.ManyToManyField(Flat, related_name='owner_flats', verbose_name='Квартиры в собственности')
+    flats = models.ManyToManyField(Flat, related_name='owners', verbose_name='Квартиры в собственности')
 
     def __str__(self):
         return f'{self.name}'
